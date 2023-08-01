a193ad7114efe666e60334136ddb04406f58f6ba
Jog Giri <42950079+JogGiri@users.noreply.github.com>
2023-08-01 21:19:41 +0530

    Create readable_large_numbers.py

---
+++ readable_large_numbers.py
@@ -0,0 +1,6 @@
+num = 100**4 
+print(num) #output 100000000 diffuct to read
+print(f'{num:,}') #output ->100,000,000 more readable
+
+num2 = 100_000_000 # we can use underscore
+print(num2) # output -> 100000000
